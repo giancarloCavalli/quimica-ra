4e40353d22c85c21018825f505fd3035600bd4dd
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2023-11-20 12:56:21 -0300

    style: attempt to make the eletrons rotation stabler (relating to the horizontal axis when changing the camera angles)

--- Assets/Scripts/Utilities/Motion.cs
+++ Assets/Scripts/Utilities/Motion.cs
@@ -4,6 +4,10 @@ public static class Motion
 {
   public static void Rotate(GameObject objectToRotate, GameObject referenceObject, float degreesPerSecond)
   {
-    objectToRotate.transform.RotateAround(referenceObject.transform.position, Vector3.up, degreesPerSecond * Time.deltaTime);
+    // Obtém o vetor up no espaço local do objeto
+    Vector3 localUp = objectToRotate.transform.InverseTransformDirection(Vector3.up);
+
+    // Rotaciona em relação ao vetor up local
+    objectToRotate.transform.RotateAround(referenceObject.transform.position, localUp, degreesPerSecond * Time.deltaTime);
   }
 }
